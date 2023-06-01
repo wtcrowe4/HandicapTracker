b1fd7b34595ad80f8556f31096f86d8e002eb078
Thomas Crowe <wcrowe@g.clemson.edu>
2023-06-01 08:09:12 -0400

    home page created

---
+++ HandicapTracker/HandicapTracker/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.ComponentModel;
+
+namespace HandicapTracker.Views.ViewModels
+{
+    public partial class HomePageViewModel : BaseViewModel
+    {
+        
+    }
+}
---
+++ HandicapTracker/HandicapTracker/Views/HomePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HandicapTracker.Views.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePagePage : ContentPage
+    {
+        public HomePagePage()
+        {
+            InitializeComponent();
+            BindingContext = new HomePageViewModel();
+        }
+    }
+}
