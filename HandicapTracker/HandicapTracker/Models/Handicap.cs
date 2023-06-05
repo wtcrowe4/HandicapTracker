7721d1094b61b884f3712b394f8a73ee7aa30641
Thomas Crowe <wcrowe@g.clemson.edu>
2023-06-05 13:31:44 -0400

    Created handicap model

---
+++ HandicapTracker/HandicapTracker/Models/Handicap.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandicapTracker.Models
+{
+    internal class Handicap
+    {
+        public double Value { get; set; }
+    }
+}
