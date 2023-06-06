bc879e739a65eb55091b0542c79f915166df3c75
Thomas Crowe <wcrowe@g.clemson.edu>
2023-06-06 08:18:11 -0400

    Working on model

--- HandicapTracker/HandicapTracker/Models/Handicap.cs
+++ HandicapTracker/HandicapTracker/Models/Handicap.cs
@@ -9,5 +9,20 @@ namespace HandicapTracker.Models
     internal class Handicap
     {
         public double Value { get; set; }
+
+        public List<Round> Rounds { get; set; }
+    }
+
+    internal class Round
+    {
+        public DateTime Date { get; set; }
+
+        public int Score { get; set; }
+
+        public int CourseRating { get; set; }
+
+        public int SlopeRating { get; set; }
+
+        public double Differential { get; set; }
     }
 }
