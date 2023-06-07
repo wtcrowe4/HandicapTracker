6f98b4bdf63c159a5d5776b1a9d4bcadb444a30d
Thomas Crowe <wcrowe@g.clemson.edu>
2023-06-07 11:58:34 -0400

    working on handicap model

--- HandicapTracker/HandicapTracker/Models/Handicap.cs
+++ HandicapTracker/HandicapTracker/Models/Handicap.cs
@@ -11,6 +11,44 @@ namespace HandicapTracker.Models
         public double Value { get; set; }
 
         public List<Round> Rounds { get; set; }
+
+        public List<Score> Scores { get; set; }
+
+        public List<Score> Differentials { get; set; }
+
+        /// <summary>
+        /// calculates the handicap value from the list of scores and differentials
+        /// </summary>
+        /// <returns>the resulting handicap</returns>
+        public double CalculateHandicapValue()
+        {
+            if (Differentials.Count < 5)
+            {
+                //return a handicap value of 0 if less than 5 Differentials exist
+                return 0;
+            }
+
+            var sortedDifferentials = Differentials.OrderBy(d => d.Value).ToList();
+
+            int count = 0;
+            double total = 0;
+
+            for (int x = 0; x < Differentials.Count; x++)
+            {
+                if (x > 19)
+                {
+                    break; // only use the lowest 20% of a user's differentials.
+                }
+
+                count++;
+                total += sortedDifferentials[x].Value;
+            }
+
+            double averageDifferential = (total / count);
+
+            return Math.Round((averageDifferential * 0.96), 1);
+        }
+
     }
 
     internal class Round
@@ -24,5 +62,15 @@ namespace HandicapTracker.Models
         public int SlopeRating { get; set; }
 
         public double Differential { get; set; }
+    }
+
+    internal class Score
+    {
+        public int Value { get; set; }
+        public int AdjustedValue { get; set; }
+
+        public int CourseRating { get; set; }
+        public int SlopeRating { get; set;}
+
     }
 }
